a1c5d1d7019774d1f791e2d9651d2220f318735d
zrpnr <peter.weber@acquia.com>
2021-01-25 10:49:02 -0700

    Export SimpleUploadAdapter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import DrupalImage from './drupalimage';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 export default {
   DrupalImage,
+  SimpleUploadAdapter,
 };
